063a09c0a4544125f5476393fa6b9ce58cf7ddac
Jefferson da Silva <je_jss@hotmail.com>
2022-03-12 23:40:55 -0300

    Project Created

---
+++ backend/src/test/java/com/jeffdev/devmovie/DevmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffdev.devmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
